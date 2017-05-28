f24bf0d80d64a0660f00badf814f6f0c9e2293d3
Lucas Tecot <lucastecot@gmail.com>
2017-05-28 13:54:21 -0700

    side by side works

--- segmentImage.py
+++ segmentImage.py
@@ -4,8 +4,13 @@ from matplotlib import pyplot
 from six.moves import cPickle as pickle
 from parameters import *
 import random
+import tensorflow as tf
 
 #Program to run and vizualize a segmentation on a image using the saved model
+
+numPatchSamples = 0
+numImgSamples = 0
+numTestSamples = 3
 
 #Testing the dataset
 
@@ -18,12 +23,14 @@ with open(pickle_file, 'rb') as f:
 	valid_features = save['valid_features']
 	valid_labels = save['valid_labels']
 
-#print(train_features.shape)
-#print(train_labels.shape)
-#print(valid_features.shape)
-#print(valid_labels.shape)
+pickle_file = 'lesion.pickle'
 
-for i in range(0,10):#random.sample(range(0, 10), 10):
+with open(pickle_file, 'rb') as f:
+	save = pickle.load(f)
+	features = save['features']
+	labels = save['labels']
+
+for i in random.sample(range(0, 10), numPatchSamples):
 	img1 = valid_features[i, :, :, 0]
 	img2 = valid_labels[i, :, :, 0]
 	img3 = valid_labels[i, :, :, 1]
@@ -41,13 +48,9 @@ with open(pickle_file, 'rb') as f:
 	features = save['features']
 	labels = save['labels']
 
-#print(train_features.shape)
-#print(train_labels.shape)
-#print(valid_features.shape)
-#print(valid_labels.shape)
 
-for i in random.sample(range(0, 13), 0):
-	for z in random.sample(range(0, 20), 0):
+for i in random.sample(range(0, 13), numImgSamples):
+	for z in random.sample(range(0, len(features[i])-1), 1):
 		img = features[i][z]
 		pyplot.imshow(img)
 		pyplot.show()
@@ -57,5 +60,46 @@ for i in random.sample(range(0, 13), 0):
 
 
 #Testing the model
+modelName = "lesion_model"
+step = 1500
 
-#To be continued!!!!
+with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)) as session:
+	new_saver = tf.train.import_meta_graph("./" + modelName + "-" + str(step) + ".meta")
+	new_saver.restore(session, tf.train.latest_checkpoint('./'))
+	session.run(tf.global_variables_initializer())
+	#print(len(features))
+	for i in random.sample(range(0, 13), numTestSamples):
+		print(i)
+		for z in random.sample(range(0, len(features[i])), 1):
+			imgF = features[i][z]
+			imgL = labels[i][z]
+			imgPL = np.zeros(imgL.shape, dtype=np.float32)
+			xDim = (imgF.shape[0] - patch_size) // output_size
+			yDim = (imgF.shape[1] - patch_size) // output_size
+			patches = np.zeros((xDim*yDim, patch_size, patch_size, 1), dtype=np.float32)
+			#outputs = np.zeros((xDim*yDim, output_size, output_size, 2), dtype=np.float32)
+			for x in range(xDim):
+				for y in range(yDim):
+					patches[x * yDim + y, :, :, 0] = imgF[x*output_size:x*output_size+patch_size, y*output_size:y*output_size+patch_size]
+
+			graph = tf.get_default_graph()
+			tf_test_features = graph.get_tensor_by_name("features:0")
+			test_prediction = graph.get_tensor_by_name("labels:0")
+			#print(patches.shape)
+			#print(imgPL.shape)
+			feed_dict = {tf_test_features : patches}
+			outputs = session.run([test_prediction], feed_dict=feed_dict)
+			outputs = np.array(outputs).reshape((-1, output_size, output_size, 2))
+			for x in range(xDim):
+				for y in range(yDim):
+					xP = x*output_size + (patch_size - output_size) // 2
+					yP = y*output_size + (patch_size - output_size) // 2
+					#print(outputs.shape)
+					imgPL[xP:xP+output_size, yP:yP+output_size] = np.argmin(outputs[x*yDim + y], 2)#outputs[x*yDim + y, :, :, 0]
+			pyplot.subplot(131)
+			pyplot.imshow(imgF)
+			pyplot.subplot(132)
+			pyplot.imshow(imgL)
+			pyplot.subplot(133)
+			pyplot.imshow(imgPL)
+			pyplot.show()
